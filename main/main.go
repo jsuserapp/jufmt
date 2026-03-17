3de85f86603590a98b11a3740622b25832f51152
pgmsoul <pgmsoul@gmail.com>
2026-03-17 09:55:35 +0800

    add readme

--- main/main.go
+++ main/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/jsuserapp/jufmt"
+import (
+	"github.com/jsuserapp/jufmt"
+)
 
 func main() {
 	jufmt.Println("Hello World")
+	jufmt.Printf("%s %s\n", "Hello", "World")
+	jufmt.Green.Println("Hello World")
+	jufmt.Cyan.Printf("%s %s\n", "Hello", "World")
+
+	hello := jufmt.Yellow.Sprintf("%s", "Hello")
+	world := jufmt.Red.Sprintf("%s", "World")
+	jufmt.Printf("%s %s\n", hello, world)
 }
